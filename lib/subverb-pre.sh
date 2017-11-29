b06ac27c3a9df77679237873aea8056f03db9ac1
Benjamin Tietz <benjamin@micronet24.de>
2017-11-29 12:22:56 +0100

    automatically setup openssl-conffile

---
+++ lib/subverb-pre.sh
@@ -0,0 +1,11 @@
+SV_OPTION=(
+	[opensslconf]=OPENSSL_CONF
+)
+
+SV_SHORT_OPTION=(
+	[F]=OPENSSL_CONF
+)
+
+if [ -z "$OPENSSL_CONF" -a -n "$CAUTL_SYSCONFIG" ]; then
+	OPENSSL_CONF=${CAUTL_SYSCONFIG}.cnf
+fi
