52251686940330ca058d43ece3a1ef1855e31fba
Benjamin Tietz <benjamin@micronet24.de>
2018-04-21 14:07:10 +0200

    [template] provide if-then-else constructs
    This provide support for template-constructs like
    
    	<<if [ $foo -gt 42 ]>>
    		more than the answer
    	<<else>>
    		maybe the answer
    	<<end>>
    
    * nesting is supported
    * tests can be any shell-code (return-value is considered)
    * <<else>> is optional

--- lib/template.sh
+++ lib/template.sh
@@ -2,6 +2,9 @@ regex='<<([a-zA-Z_][a-zA-Z_0-9]*)>>'
 includere='^<<include ([a-zA-Z_0-9.]+)>>'
 setre='^<<([a-zA-Z_][a-zA-Z_0-9]+)=([^>]*)>>'
 spaceonlyre='^\s*$'
+ifre='(.*)<<if ([^>][^>]*)>>(.*)'
+elsere='(.*)<<else>>(.*)'
+endre='(.*)<<end>>(.*)'
 
 trap_add() {
 	cmd=$1
@@ -17,6 +20,49 @@ declare -f -t trap_add
 _parse_line() {
 	declare line="$1"
 	if [[ "$line" =~ $spaceonlyre ]]; then
+		return
+	fi
+	if [[ "$line" =~ $endre ]]; then
+		_parse_line "${BASH_REMATCH[1]}"
+		#echo "end IFSTATE: ${IFSTATE[*]}"
+		if [ ${#IFSTATE[*]} -lt 2 ]; then
+			echo "<<end>> without if!" 1>&2
+			exit 1
+		fi
+		unset IFSTATE[-1]
+		_parse_line "${BASH_REMATCH[2]}"
+		return
+	fi
+	if [[ "$line" =~ $elsere ]]; then
+		_parse_line "${BASH_REMATCH[1]}"
+		# invert current ifstate
+		if [ ${#IFSTATE[*]} -lt 2 ]; then
+			echo "<<else>> without if!" 1>&2
+			exit 1
+		fi
+		#echo "invert IFSTATE: ${IFSTATE[*]}"
+		IFSTATE[-1]="$((${IFSTATE[-2]} * ! ${IFSTATE[-1]}))"
+		#echo "invres IFSTATE: ${IFSTATE[*]}"
+		_parse_line "${BASH_REMATCH[2]}"
+		return
+	fi
+	if [[ "$line" =~ $ifre ]]; then
+		declare preline="${BASH_REMATCH[1]}"
+		declare testexp="${BASH_REMATCH[2]}"
+		declare postline="${BASH_REMATCH[3]}"
+		declare newifstate=${IFSTATE[-1]}
+		_parse_line "$preline"
+		if [[ "$newifstate" -gt 0 ]]; then
+			eval "$testexp"
+			newifstate=$((!$?))
+		fi
+		#echo "new(${#IFSTATE[*]}) IFSTATE: ${IFSTATE[*]} ($newifstate)"
+		IFSTATE[${#IFSTATE[*]}]=$newifstate
+		#echo "res(${#IFSTATE[*]}) IFSTATE: ${IFSTATE[*]}"
+		_parse_line "$postline"
+		return
+	fi
+	if [[ "${IFSTATE[-1]}" -lt 1 ]]; then
 		return
 	fi
 	if [[ "$line" =~ $includere ]]; then
@@ -42,6 +88,7 @@ _parse_line() {
 
 parse_template() {
 	BASEDIR=${1:-.}
+	declare -a IFSTATE=(1)
 	while read line; do
 		_parse_line "$line"
 	done
