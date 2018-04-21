3bb7b287120dec3f6f025bcd07af0d22d303e185
Benjamin Tietz <benjamin@micronet24.de>
2018-04-21 15:06:41 +0200

    [sign] provide certificate-path via named parameter

--- lib/subverb/selfsign.sh
+++ lib/subverb/selfsign.sh
@@ -3,5 +3,5 @@ CERT=$($GETCACONF  -k certificate)
 CAUTL_CA_ARG=-selfsign
 CAUTL_CA_EXTENSION=ca_ext
 
-sv_call_subverb sign $CSR $CERT
+sv_call_subverb sign $CSR --certificate $CERT
 
--- lib/subverb/sign.sh
+++ lib/subverb/sign.sh
@@ -1,12 +1,26 @@
 NEWCSRDIR=$($GETCACONF  -k new_certs_dir)
 PRIVKEY=$($GETCACONF -k private_key)
 
-CSR=$1
-CERT=${2-$($GETCACONF -k certs)/$CSR}
+SV_OPTION[certificate]="CERT"
+
+sv_parse_options "$@"
+
+CSR="${SV_UNPARSED[0]}"
+
+_parse_option_helper() {
+	shift
+	sv_parse_options "$@"
+}
+
+_parse_option_helper "${SV_UNPARSED[@]}"
+
 
 if [ -z "$CSR" ]; then
 	echo "Please provide the name of the CSR to sign. Optionally also the path to the CERTificate" 1>&2
 	exit 1
+fi
+if [ -z "$CERT" ]; then
+	CERT=$($GETCACONF -k certs)/$CSR
 fi
 
 openssl ca -config $OPENSSL_CONF -in $NEWCSRDIR/$CSR -out $CERT -passin file:${PRIVKEY}.pwd -extensions ${CAUTL_CA_EXTENSION-sub_ca_ext} ${CAUTL_CA_ARG}
