11b6244587da323c2a4f6724f82b14e7825fe12f
Benjamin Tietz <benjamin@micronet24.de>
2018-04-21 12:07:05 +0200

    [cmdline] --config-type option
    for selecting a different addon-configuration

--- lib/subverb-configured.sh
+++ lib/subverb-configured.sh
@@ -1,3 +1,8 @@
+if [ -n "${CAUTL_CONFIG_TYPE}" ]; then
+	parse_config "$(sv_default_dir sysconf).${CAUTL_CONFIG_TYPE}"
+	parse_config "$HOME/.${SVBASE}.${CAUTL_CONFIG_TYPE}"
+fi
+
 generate_configfile ${CAUTL_GROUP}
 
 OPENSSL_CONF=${CAUTL_GENERATED_FILE}
--- lib/subverb-pre.sh
+++ lib/subverb-pre.sh
@@ -3,11 +3,17 @@ CAUTL_GROUP=${CAUTL_GROUP:-default}
 SV_OPTION=(
 	[opensslconf]=OPENSSL_CONF
 	[group]=CAUTL_GROUP
+	[config-type]=CAUTL_CONFIG_TYPE
 )
 
 SV_SHORT_OPTION=(
 	[F]=OPENSSL_CONF
 	[g]=CAUTL_GROUP
+	[c]=CAUTL_CONFIG_TYPE
+)
+
+SV_OPTION_HELP=(
+	[CAUTL_CONFIG_TYPE]="add a suffix for alternate configuration overrides"
 )
 
 . $(sv_default_dir pkglib)/template.sh
